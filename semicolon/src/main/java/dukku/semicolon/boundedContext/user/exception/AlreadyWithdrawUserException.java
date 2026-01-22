664e91dc5d4551db391dd3b8329f3e70b7369499
이태석 <rolts1231@naver.com>
2026-01-22 14:19:48 +0900

    Feat: user 중복 삭제 예외 처리 수정

--- semicolon/src/main/java/dukku/semicolon/boundedContext/user/exception/AlreadyWithdrawUserException.java
+++ semicolon/src/main/java/dukku/semicolon/boundedContext/user/exception/AlreadyWithdrawUserException.java
@@ -1,6 +1,8 @@
 package dukku.semicolon.boundedContext.user.exception;
 
-public class AlreadyWithdrawUserException extends RuntimeException {
+import dukku.common.global.exception.ConflictException;
+
+public class AlreadyWithdrawUserException extends ConflictException {
 
     public AlreadyWithdrawUserException() {
         super("이미 탈퇴한 회원입니다.");
